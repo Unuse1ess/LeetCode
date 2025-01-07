68ab9f091fdfb851f339670fad459681408793a6
unuse1ess <895348025@qq.com>
2025-01-07 16:23:39 +0800

    problem 34

--- python_src/34.在排序数组中查找元素的第一个和最后一个位置.py
+++ python_src/34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -4,7 +4,7 @@
 # [34] 在排序数组中查找元素的第一个和最后一个位置
 #
 from typing import *
-
+from math import *
 
 # @lc code=start
 class Solution:
@@ -14,18 +14,45 @@ class Solution:
         if size == 1: return [-1, -1] if target != nums[0] else [0, 0]
 
         l, r = 0, size - 1
-        while l < r and r < size:
-            mid = (l + r) << 1
+        step = size
+        while l <= r and r < size:
+            step >>= 1
+            mid = (l + r) >> 1
             if target < nums[mid]:
                 r = mid - 1
             elif target > nums[mid]:
                 l = mid + 1
             else:
-                pass
-        
+                if step == 0:
+                    return mid, mid
+                l, r = mid, mid
+                prev = -1
+                while True:
+                    _l = l - step
+                    if _l < 0: _l = 0
+                    if nums[_l] == target:
+                        l = _l
+                    _r = r + step
+                    if _r >= size: _r = size - 1
+                    if nums[_r] == target:
+                        r = _r
+                    if step == 1 and r - l == prev:
+                        break
+                    prev = r - l
+                    step = (step >> 1) | (step & 1)
+            
+                return l, r
+        return -1, -1
 # @lc code=end
 
 s = Solution()
 print(s.searchRange(nums = [5,7,7,8,8,10], target = 8))
 print(s.searchRange(nums = [5,7,7,8,8,10], target = 6))
 print(s.searchRange(nums = [], target = 0))
+print(s.searchRange(nums = [5,7,7,8,8,10], target = 7))
+print(s.searchRange(nums = [5,7,7,8,8,8], target = 8))
+print(s.searchRange(nums = [5,7,7,8,8,10,10], target = 10))
+print(s.searchRange(nums = [5,5,7,8,8,8,10], target = 5))
+
+## [15,20]
+print(s.searchRange([1,1,1,2,4,4,4,5,5,5,5,6,7,8,8,9,9,9,9,9,9,10], 9))
