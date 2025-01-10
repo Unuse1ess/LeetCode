524acd0827afb00f2d7d06a1efe8e7f1119a75f0
unuse1ess <895348025@qq.com>
2025-01-10 16:24:02 +0800

    problem4 AC

--- python_src/4.寻找两个正序数组的中位数.py
+++ python_src/4.寻找两个正序数组的中位数.py
@@ -9,53 +9,62 @@ from math import *
 # @lc code=start
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        def find_kth(l1: int, r1: int, l2: int, r2: int, k: int) -> float:
+        m, n = len(nums1), len(nums2)
+
+        def find_kth(l1: int, l2: int, k: int) -> float:
             while True:
                 step = k >> 1
-                if step == 0: step = 1
-                s1, s2 = r1 - l1, r2 - l2
-                i1, i2 = l1 + step - 1, l2 + step - 1
-                if s1 == 0:
-                    v1 = None
-                    d1 = 0
+                step = 1 if step == 0 else step
+
+                k1 = l1 + step - 1
+                k2 = l2 + step - 1
+
+                if k1 < m:
+                    v1 = nums1[k1]
+                    # step1 = step + (1 if step != 1 else 0)
+                    step1 = step
                 else:
-                    # v1 = nums1[r1 - 1] if i1 >= s1 else nums1[i1]
-                    d1 = r1 - 1 if i1 >= s1 else i1
-                    v1 = nums1[d1]
-                    d1 = d1 - l1
-                if s2 == 0:
-                    v2 = None
-                    d2 = 0
+                    if l1 < m:
+                        v1 = nums1[m - 1]
+                        step1 = m - l1
+                    else:
+                        v1 = None
+                        step1 = 0
+                if k2 < n:
+                    v2 = nums2[k2]
+                    # step2 = step + (1 if step != 1 else 0)
+                    step2 = step
                 else:
-                    # v2 = nums2[r2 - 1] if i2 >= s2 else nums2[i2]
-                    d2 = r2 - 1 if i2 >= s2 else i2
-                    v2 = nums2[d2]
-                    d2 = d2 - l2
+                    if l2 < n:
+                        v2 = nums2[n - 1]
+                        step2 = n - l2
+                    else:
+                        v2 = None
+                        step2 = 0
+
                 if v1 is not None and v2 is not None:
                     if v1 < v2:
                         if k == 1:
                             return v1
-                        k -= d1 + 1
-                        l1 += d1 + 1
+                        k -= step1
+                        l1 += step1
                     else:
                         if k == 1:
                             return v2
-                        k -= d2 + 1
-                        l2 += d2 + 1
-                elif v1 is not None and v2 is None:
-                    return nums1[k]
-                elif v1 is None and v2 is not None:
-                    return nums2[k]
-                
+                        k -= step2
+                        l2 += step2
+                elif v1 is None:
+                    return nums2[l2 + k - 1]
+                else:
+                    return nums1[l1 + k - 1]
 
-        m, n = len(nums1), len(nums2)
         tmp = (m + n) >> 1
 
         if (m + n) & 1 == 1:
-            return find_kth(0, m, 0, n, tmp)
+            return find_kth(0, 0, tmp + 1)
         else:
-            return (find_kth(0, m, 0, n, tmp) +
-                    find_kth(0, m, 0, n, tmp + 1)) / 2
+            return (find_kth(0, 0, tmp) +
+                    find_kth(0, 0, tmp + 1)) / 2
 
         
 # @lc code=end
@@ -63,7 +72,12 @@ class Solution:
 # 1,3,5  2,4,6
 
 s = Solution()
-# print(s.findMedianSortedArrays([1,3], [2]))
-# print(s.findMedianSortedArrays([1,2], [3,4]))
-# print(s.findMedianSortedArrays([1,2,4], [3,5,6]))
+print(s.findMedianSortedArrays([1,3], [2]))
+print(s.findMedianSortedArrays([1,2], [3,4]))
+print(s.findMedianSortedArrays([1,2,4], [3,5,6]))
 print(s.findMedianSortedArrays(list(range(5)), list(range(5, 10))))
+print(s.findMedianSortedArrays([0,0,0,0,0], [-1,0,0,0,0,0,1]))
+print(s.findMedianSortedArrays([], [1]))
+print(s.findMedianSortedArrays([], [1,2]))
+print(s.findMedianSortedArrays([], [1,2,3]))
+print(s.findMedianSortedArrays([], [1,2,3,4]))
